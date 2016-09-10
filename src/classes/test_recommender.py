93183d9f703eb5d71ba2c8ecd1e41e794d4837ef
lampros <lampros@lampross-MacBook-Pro.local>
2016-09-10 20:37:40 +0200

    Fixed name

--- src/classes/test_recommender.py
+++ src/classes/test_recommender.py
@@ -5,12 +5,12 @@ import recommender
 
 class TestRecommenderMethods(unittest.TestCase):
 
-    def test_naive_type(self):
+    def test_array_average(self):
         global_avg = 1
         array = np.array([[2, 4], [1, 5], [67, 2], [23, 1], [23, 4]])
         total_entries = 100
         test = recommender.Recommender(None)
-        result = test.naive_type(array, total_entries)
+        result = test.array_average(array, total_entries)
         self.assertEqual(len(result), total_entries + 1)
         self.assertEqual(result[23], 2.5)
         self.assertEqual(result[2], 4)
